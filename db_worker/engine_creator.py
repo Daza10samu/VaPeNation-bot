65661b1f84dbd29d5d1c3c970ef49255fa0b890c
Daza10Samu <andreyhigher@gmail.com>
2020-11-22 11:43:36 +0300

    Change db connection engine

--- db_worker/engine_creator.py
+++ db_worker/engine_creator.py
@@ -11,7 +11,7 @@ def create(environ):
     if 'MYSQL_DBNAME' not in environ:
         engine = create_engine('sqlite:///users.db', echo=False)
     else:
-        engine = create_engine(f'mysql+mysqldb://{environ["MYSQL_USERNAME"]}:{environ["MYSQL_PASSWORD"]}@localhost:3306/{environ["MYSQL_DBNAME"]}', pool_pre_ping=True)
+        engine = create_engine(f'mysql+mysqlconnector://{environ["MYSQL_USERNAME"]}:{environ["MYSQL_PASSWORD"]}@localhost:3306/{environ["MYSQL_DBNAME"]}', pool_pre_ping=True)
     from . import __all_models
     SqlAlchemyBase.metadata.create_all(engine)
     __maker = sessionmaker(bind=engine)
